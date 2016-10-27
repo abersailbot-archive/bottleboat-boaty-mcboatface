f4468d549bc4fedb39d2313292eded4ae0ecf92e
CloudCoppola <clc20@aber.ac.uk>
2016-10-27 11:05:20 +0100

    Update: Servo code

--- TheControlSystem/TheControlSystem.ino
+++ TheControlSystem/TheControlSystem.ino
@@ -46,3 +46,28 @@ void loop() {
   
 }
 
+include <Servo.h>
+
+Servo myservo;  // create servo object to control a servo
+// twelve servo objects can be created on most boards
+
+int midpoint = 60;
+
+int pos = midpoint;   // variable to store the servo position
+                      // should never pass +/- 30 from the midpoint
+
+void setup() {
+  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+}
+
+void loop() {
+  for (pos = 30; pos <= 90; pos += 1) { // goes from 30 degrees to 90 degrees
+    // in steps of 1 degree
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
+  for (pos = 90; pos >= 30; pos -= 1) { // goes from 90 degrees to 30 degrees
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
+}
